efe4060a321a624278818e59a38c8f58fe8319f1
smarttommyau <smarttommyau@gmail.com>
2025-01-08 10:51:13 +0800

    Feat: header for inspire3d_display

---
+++ ch32v003fun/inspire3d_display.h
@@ -0,0 +1,100 @@
+#ifndef INSPIRE3D_DISPLAY
+#define INSPIRE3D_DISPLAY
+
+#include <stdint.h>
+#include <stdlib.h>
+#define WS2812BSIMPLE_IMPLEMENTATION
+#include "ws2812b_simple.h"
+#include "ch32v003_i2c.h"
+
+#ifdef INSPIRE3D_DISPLAY_COMMON_COLOR
+#include "colors.h"
+#endif
+
+
+struct Inspire3D_Display {
+    uint8_t data[125][3];
+    // 0-24 : back layer
+    // ...
+    // 100-124 : front layer
+    // three values {g,r,b}
+    GPIO_TypeDef * port; // for instance, GPIOA
+    int pin; // for instance, A2
+    float brightness = 1.0;
+};
+
+struct Inspire3D_Color{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
+
+Inspire3D_Display * Inspire3D_Display_Init(GPIO_TypeDef * port, int pin);
+
+uint8_t Inspire3D_Display_Coords2Index(uint8_t x, uint8_t y, uint8_t z);
+
+void Inspire3D_Display_Index2Coords(uint8_t index, uint8_t * x, uint8_t * y, uint8_t * z);
+
+Inspire3D_Color Inspire3D_Color_setRGB(uint8_t r, uint8_t g, uint8_t b);
+
+void Inspire3D_Color_calculateBrightness(Inspire3D_Color * color);
+
+void Inspire3D_Display_SetColor(Inspire3D_Display * display, uint8_t index, Inspire3D_Color color);
+
+void Inspire3D_Display_SetBGColor(Inspire3D_Display * display, Inspire3D_Color color);
+
+void Inspire3D_Display_Update(Inspire3D_Display * display);
+
+void Inspire3D_Display_Clear(Inspire3D_Display * display);
+
+
+Inspire3D_Display * Inspire3D_Display_Init(GPIO_TypeDef * port, int pin){
+    Inspire3D_Display * display = (Inspire3D_Display *)malloc(sizeof(Inspire3D_Display));
+    display->port = port;
+    display->pin = pin;
+    return display;
+}
+
+uint8_t Inspire3D_Display_Coords2Index(uint8_t x, uint8_t y, uint8_t z){
+        return x + y*5 + z*25;
+}
+
+void Inspire3D_Display_Index2Coords(uint8_t index, uint8_t * x, uint8_t * y, uint8_t * z){
+    *x = index % 5;
+    *y = (index / 5) % 5;
+    *z = index / 25;
+}
+
+Inspire3D_Color Inspire3D_Color_setRGB(uint8_t r, uint8_t g, uint8_t b){
+    Inspire3D_Color color = {r, g, b};
+    return color;
+}
+
+void Inspire3D_Color_calculateBrightness(Inspire3D_Color * color,float brightness){
+    color->r = color->r * brightness;
+    color->g = color->g * brightness;
+    color->b = color->b * brightness;
+}
+
+
+void Inspire3D_Display_SetColor(Inspire3D_Display * display, uint8_t index, Inspire3D_Color color){
+    Inspire3D_Color_calculateBrightness(&color, display->brightness);
+    display->data[index][0] = color.g;
+    display->data[index][1] = color.r;
+    display->data[index][2] = color.b;
+}
+void Inspire3D_Display_SetBGColor(Inspire3D_Display * display, Inspire3D_Color color){
+    Inspire3D_Color_calculateBrightness(&color, display->brightness);
+    for(int i = 0; i < 125; i++){
+        display->data[i][0] = color.g;
+        display->data[i][1] = color.r;
+        display->data[i][2] = color.b;
+    }
+}
+
+
+void Inspire3D_Display_Update(Inspire3D_Display * display){
+    WS2812BSimpleSend(display->port, display->pin, (uint8_t *)display->data, 125 * 3);
+}
+
+#endif
---
+++ data/colors.h
@@ -0,0 +1,18 @@
+// provide common colors for inspire3d color
+#ifdef INSPIRE3D_DISPLAY_COMMON_COLOR
+
+struct Inspire3D_Color Inspire3D_Color_Black = {0,0,0};
+struct Inspire3D_Color Inspire3D_Color_Red = {255,0,0};
+struct Inspire3D_Color Inspire3D_Color_Green = {0,255,0};
+struct Inspire3D_Color Inspire3D_Color_Blue = {0,0,255};
+struct Inspire3D_Color Inspire3D_Color_Yellow = {255,255,0};
+struct Inspire3D_Color Inspire3D_Color_Cyan = {0,255,255};
+struct Inspire3D_Color Inspire3D_Color_Magenta = {255,0,255};
+struct Inspire3D_Color Inspire3D_Color_White = {255,255,255};
+struct Inspire3D_Color Inspire3D_Color_Gray = {128,128,128};
+struct Inspire3D_Color Inspire3D_Color_Brown = {160,128,96};
+struct Inspire3D_Color Inspire3D_Color_Orange = {255,160,16};
+struct Inspire3D_Color Inspire3D_Color_Pink = {255,192,203};
+struct Inspire3D_Color Inspire3D_Color_Purple = {160,32,255};
+
+#endif
