5e08670b643a27d17c32090f949f42384f352cd6
smarttommyau <smarttommyau@gmail.com>
2025-01-09 15:53:16 +0800

    Feat: select color to set color

--- color_selector/color_selector.c
+++ color_selector/color_selector.c
@@ -52,35 +52,55 @@ int main(void) {
     Inspire3D_Display_Clear(display);// reset data and update
     Inspire3D_Display_SetBrightness(display, 0.05);
     show_color_pallete(display);
+    uint8_t mode = 0; //0: select; 1: selected
     while(1){
         // read ABCD key
-        uint16_t abcd_reading = abcd_key_read_ADC();
-        uint16_t arrow_reading = arrow_key_read_ADC();
-        ARROW_KEY arrow = arrow_key_down(arrow_reading);
-        if(arrow == ARROW_UP){
-            y = (y + 1) % 5;
-        } else if(arrow == ARROW_DOWN){
-            y = (y - 1) % 5;
-        } else if(arrow == ARROW_RIGHT){
-            x = (x + 1) % 5;
-        } else if(arrow == ARROW_LEFT){
-            x = (x - 1) % 5;
+        uint16_t abcd_reading   = abcd_key_read_ADC();
+        uint16_t arrow_reading  = arrow_key_read_ADC();
+        ARROW_KEY arrow         = arrow_key_down(arrow_reading);
+        ABCD_KEY abcd           = abcd_key_down(abcd_reading);
+        if(mode == 0){
+            if(arrow == ARROW_UP){
+                y = (y + 1) % 5;
+            } else if(arrow == ARROW_DOWN){
+                y = (y - 1) % 5;
+            } else if(arrow == ARROW_RIGHT){
+                x = (x + 1) % 5;
+            } else if(arrow == ARROW_LEFT){
+                x = (x - 1) % 5;
+            }
+            // read arrow key
+            if(abcd == ABCD_A){
+                z = (z + 1) % 5;
+            } else if(abcd == ABCD_B){
+                z = (z - 1) % 5;
+            }
         }
-        // read arrow key
-        if(abcd_reading >= IDC_ABCD_A_L_D2 && abcd_reading <= IDC_ABCD_A_U_D2){
-            z = (z + 1) % 5;
-        } else if(abcd_reading >= IDC_ABCD_B_L_D2 && abcd_reading <= IDC_ABCD_B_U_D2){
-            z = (z - 1) % 5;
+        // select with CD keys
+        if(abcd == ABCD_C||abcd == ABCD_D){
+            if(mode == 0){
+                mode = 1;
+                Inspire3D_Display_SetBGColor(display, Inspire3D_Color_setRGB(y*50,x*50,z*50));
+                Inspire3D_Display_Update(display);
+            } else {
+                mode = 0;
+                show_color_pallete(display);
+            }
         }
-        // set color
-        uint8_t index = Inspire3D_Display_Coords2Index(x,y,z);
-        // Inspire3D_Display_SetColor(display, index, Inspire3D_Color_setRGB(y*50,x*50,z*50));
-        printf("x: %d, y: %d, z: %d adc: %d\n", x, y, z, abcd_reading);
-        Inspire3D_Display_SetColor(display, index, Inspire3D_Color_Black);
-        Inspire3D_Display_Update(display);
-        Delay_Ms(100);
-        Inspire3D_Display_SetColor(display, index, Inspire3D_Color_setRGB(y*50,x*50,z*50));
-        Inspire3D_Display_Update(display);
+        if(mode == 0){
+            // blink color
+            uint8_t index = Inspire3D_Display_Coords2Index(x,y,z);
+            // Inspire3D_Display_SetColor(display, index, Inspire3D_Color_setRGB(y*50,x*50,z*50));
+            printf("x: %d, y: %d, z: %d adc: %d\n", x, y, z, abcd_reading);
+            Inspire3D_Display_SetColor(display, index, Inspire3D_Color_Black);
+            Inspire3D_Display_Update(display);
+            Delay_Ms(100);
+            Inspire3D_Display_SetColor(display, index, Inspire3D_Color_setRGB(y*50,x*50,z*50));
+            Inspire3D_Display_Update(display);
+        }else{
+            printf("x: %d, y: %d, z: %d adc: %d\n", x, y, z, abcd_reading);
+
+        }
         Delay_Ms(100);
     }
 }
