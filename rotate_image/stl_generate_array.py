f713dca80073bb5ce251b095b96a33cc4772031a
smarttommyau <smarttommyau@gmail.com>
2025-01-20 14:48:15 +0800

    Feat: refine render algorithm

--- rotate_image/stl_generate_array.py
+++ rotate_image/stl_generate_array.py
@@ -1,7 +1,8 @@
-import numpy
+import numpy as np
+# np.float_ = np.float64 # to fix the error: AttributeError: module 'numpy' has no attribute 'float_'
 from stl import mesh
 import sys
-
+import raster_geometry as rg
 
 # Align rounding function with the c program
 # From `rotate_image.c`:
@@ -27,12 +28,12 @@ print(f"Dimension: {dimension}")
 filename = input("Enter the filename of the stl file: ")
 # filename = "basic_sphere.stl"
 mesh_data = mesh.Mesh.from_file(filename)
-max_x = numpy.max(mesh_data.x)
-min_x = numpy.min(mesh_data.x)
-max_y = numpy.max(mesh_data.y)
-min_y = numpy.min(mesh_data.y)
-max_z = numpy.max(mesh_data.z)
-min_z = numpy.min(mesh_data.z)
+max_x = np.max(mesh_data.x)
+min_x = np.min(mesh_data.x)
+max_y = np.max(mesh_data.y)
+min_y = np.min(mesh_data.y)
+max_z = np.max(mesh_data.z)
+min_z = np.min(mesh_data.z)
 print(f"max_x: {max_x}, min_x: {min_x}, max_y: {max_y}, min_y: {min_y}, max_z: {max_z}, min_z: {min_z}")
 
 # map mesh coords to dimension x dimension x dimension grid
@@ -58,22 +59,62 @@ with open('stl_data_template.h', 'r') as file:
 # set NUM_NODES by replacing TO_BE_REPLACE_WITH_NUM_NODES to the number of nodes
 cache = {} # skip duplicate points
 nodes_count = 0
+
+def full_triangle(a, b, c):
+    ab = rg.bresenham_line(a, b, endpoint=True)
+    for x in set(ab):
+        yield from rg.bresenham_line(c, x, endpoint=True)
+
+coords_count = 0
 for vector in mesh_data.vectors:
-    # just write the 3 points in the triangle to the file
-    for i in range(3):
-        x, y, z = get_grid_coords_from_mesh_coords(vector[i][0], vector[i][1], vector[i][2])
-        # 1dp
-        x = round(x, 1)
-        y = round(y, 1)
-        z = round(z, 1)
-        # use int(.) so that only one point is save for each render point
-        if(f"{my_round(x)},{my_round(y)},{my_round(z)}" in cache):
+    a,b,c = vector
+    a = get_grid_coords_from_mesh_coords(a[0], a[1], a[2])
+    a = (a[0]*10, a[1]*10, a[2]*10)
+    a = (my_round(a[0]), my_round(a[1]), my_round(a[2]))
+    b = get_grid_coords_from_mesh_coords(b[0], b[1], b[2])
+    b = (b[0]*10, b[1]*10, b[2]*10)
+    b = (my_round(b[0]), my_round(b[1]), my_round(b[2]))
+    c = get_grid_coords_from_mesh_coords(c[0], c[1], c[2])
+    c = (c[0]*10, c[1]*10, c[2]*10)
+    c = (my_round(c[0]), my_round(c[1]), my_round(c[2]))
+    coords = set(full_triangle(a, b, c))
+    coords_count += len(coords)
+    for x, y, z in coords:
+        x= float(x)/10 
+        y= float(y)/10
+        z= float(z)/10
+        query = f"{my_round(x)},{my_round(y)},{my_round(z)}"
+        if(query in cache):
+            cache[query][0] += x
+            cache[query][1] += y
+            cache[query][2] += z
+            cache[query][3] += 1
             continue
-        cache[f"{my_round(x)},{my_round(y)},{my_round(z)}"] = True
+        cache[query] = [x, y, z, 1]
         nodes_count += 1
-        data += f"{{.x = {x:.1f}, .y = {y:.1f}, .z = {z:.1f}}},\n"
+for key, value in cache.items():
+    x = value[0]/value[3]
+    y = value[1]/value[3]
+    z = value[2]/value[3]
+    data += f"{{.x = {x:.1f}, .y = {y:.1f}, .z = {z:.1f}}},\n"
+    # old algorithm
+    # just write the 3 points in the triangle to the file
+    # for i in range(3):
+    #     x, y, z = get_grid_coords_from_mesh_coords(vector[i][0], vector[i][1], vector[i][2])
+    #     # 1dp
+    #     x = round(x, 1)
+    #     y = round(y, 1)
+    #     z = round(z, 1)
+    #     # use int(.) so that only one point is save for each render point
+    #     if(f"{my_round(x)},{my_round(y)},{my_round(z)}" in cache):
+    #         continue
+    #     cache[f"{my_round(x)},{my_round(y)},{my_round(z)}"] = True
+    #     nodes_count += 1
+    #     data += f"{{.x = {x:.1f}, .y = {y:.1f}, .z = {z:.1f}}},\n"
 data += "};\n"
 data = data.replace("TO_BE_REPLACE_WITH_NUM_NODES", str(nodes_count))
+print(f"Number of vectors: {len(mesh_data.vectors)}")
+print(f"Number of coords: {coords_count}")
 print(f"Number of nodes: {nodes_count}")
 # calculate Center x,y,z
 data = data.replace("TO_BE_REPLACE_WITH_CENTER_X",str((dimension[0] - 1)/2))
